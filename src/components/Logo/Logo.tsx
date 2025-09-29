f8f98097ae24d23e22837a0276c6044bae1b4a40
Tarık Kavaz <tarikkavaz@gmail.com>
2025-09-29 20:20:26 +0300

    Update logo source path in Logo component for correct asset loading
    - Changed the logo source path from "logo.svg" to "/logo.svg" to ensure proper loading of the logo asset in the application.

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -9,7 +9,7 @@ export const Logo = ({ className }: { className?: string }) => {
       className={cn("w-auto h-auto", className)}
       // className={cn("invert dark:invert-0", className)}
       // src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
-      src="logo.svg"
+      src="/logo.svg"
     />
   )
 }
